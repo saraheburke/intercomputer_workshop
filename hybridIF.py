a4b37b69bdb05592fe9398613ba2ce1ee8597ddf
saraheburke <>
2021-08-04 10:30:11 -0400

    added feature to delete IF items

--- hybridIF.py
+++ hybridIF.py
@@ -18,67 +18,128 @@ import pandas as pd
 import flywheel 
 fw=flywheel.Client()
 
-#bidsdir = '/project/ftdc_hcp/fmriBids/test06'
-#files = os.listdir(bidsdir)
 project='HCPMultiCenter'
 
-df = {"INDDID":["070001","070001"], "Session":["MR1","MR2"]}
+df = {"INDDID":["070001","070001","800172","800177"], "Session":["MR1","MR2","MR1","MR1"]}
 df = pd.DataFrame(df)
 
 subs = df["INDDID"].to_list()
 sesslist = df["Session"].to_list()
 
 fullfiles=[]
-for s in range(len(subs)):     
+for s in range(len(subs)):   
     sessPath='pennftdcenter/' + project + '/' + subs[s] + '/' + sesslist[s]
     session=fw.lookup(sessPath)
-    acqs=[a for a in session.acquisitions() if "SpinEchoFieldMap" in a.label]
-    for acq in acqs:
-        acq=acq.reload()
-        for file in acq.files:
-            print(file.info)
-            if file.type=='nifti' and file.info['PhaseEncodingDirection']=='j-' and "BIDS" in file.info.keys():
+    for acq in session.acquisitions():
+        if "SpinEchoFieldMap" in acq.label:
+            acq=acq.reload()
+            for file in acq.files:
+                if file.type=="nifti" and file.info['PhaseEncodingDirection']=='j-' and "BIDS" in file.info.keys():
+                    fullfiles.append(file)
+                    fx=sorted(fullfiles, key=lambda i:i['info']['BIDS']['Filename'])
+    yy=fx.pop()
+    print(yy.info['BIDS']['Filename'])
+    yz=yy.get('info',{})       
+    
+if "IntendedFor" in yy.info:
+        yz['IntendedFor'].append(["func/{}_{}_task-gambling_dir-PA_bold.nii.gz".format(subs[s],sesslist[s]), "func/{}_{}_task-WM_dir-PA_bold.nii.gz".format(subs[s],sesslist[s])])
+    else:
+        yz.update({'IntendedFor':["func/{}_{}_task-gambling_dir-PA_bold.nii.gz".format(subs[s],sesslist[s]), "func/{}_{}_task-WM_dir-PA_bold.nii.gz".format(subs[s],sesslist[s])]})
+    yy.update_info(yz)
+        if file.type=='nifti' and file.info['PhaseEncodingDirection']=='j-' and "BIDS" in file.info.keys():
+                fullfiles.append(file)
+                fx=sorted(fullfiles, key=lambda i:i['info']['BIDS']['Filename'])
+    
+    
+    
+    
+    if "IntendedFor" in file.info.keys():
+                        
+    else:
+                        
+                
+                
+  
+######check fields
+for s in range(len(subs)):   
+    sessPath='pennftdcenter/' + project + '/' + subs[s] + '/' + sesslist[s]
+    session=fw.lookup(sessPath)
+    for acq in session.acquisitions():
+        if "SpinEchoFieldMap" in acq.label:
+    #acqs=[a for a in session.acquisitions() if "SpinEchoFieldMap" in a.label]
+            acq=acq.reload()
+            for file in acq.files:
+                if file.type=="nifti":
+                    yz=yy.get('info',{})
+    if "IntendedFor" in yy.info:
+        yz['IntendedFor'].append(["func/{}_{}_task-gambling_dir-PA_bold.nii.gz".format(subs[s],sesslist[s]), "func/{}_{}_task-WM_dir-PA_bold.nii.gz".format(subs[s],sesslist[s])])
+    else:
+        yz.update({'IntendedFor':["func/{}_{}_task-gambling_dir-PA_bold.nii.gz".format(subs[s],sesslist[s]), "func/{}_{}_task-WM_dir-PA_bold.nii.gz".format(subs[s],sesslist[s])]})
+    yy.update_info(yz)
+        if file.type=='nifti' and file.info['PhaseEncodingDirection']=='j-' and "BIDS" in file.info.keys():
                 fullfiles.append(file)
                 fx=sorted(fullfiles, key=lambda i:i['info']['BIDS']['Filename'])
     yy=fx.pop()
     print(yy.info['BIDS']['Filename'])
     yz=yy.get('info',{})
     if "IntendedFor" in yy.info:
-        yz['IntendedFor'].append('testno')
+        yz['IntendedFor'].append(["func/{}_{}_task-gambling_dir-PA_bold.nii.gz".format(subs[s],sesslist[s]), "func/{}_{}_task-WM_dir-PA_bold.nii.gz".format(subs[s],sesslist[s])])
     else:
-        yz.update({'IntendedFor':['testpos']})
+        yz.update({'IntendedFor':["func/{}_{}_task-gambling_dir-PA_bold.nii.gz".format(subs[s],sesslist[s]), "func/{}_{}_task-WM_dir-PA_bold.nii.gz".format(subs[s],sesslist[s])]})
     yy.update_info(yz)
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    if "IntendedFor" in file.info.keys():
+                        print('yes', file.info['BIDS']['Filename'])
+                    else:
+                        print('no',file.info['BIDS']['Filename'])
+                
+                         
+                
+                
     
-   
 
+#remove IF field
+for s in range(len(subs)):   
+    sessPath='pennftdcenter/' + project + '/' + subs[s] + '/' + sesslist[s]
+    session=fw.lookup(sessPath)
+    for acq in session.acquisitions():
+        if "SpinEchoFieldMap" in acq.label:
+    #acqs=[a for a in session.acquisitions() if "SpinEchoFieldMap" in a.label]
+            acq=acq.reload()
+            for file in acq.files:
+                if file.type=="nifti":
+                    if "IntendedFor" in file.info.keys():
+                        print('yes', file.info['BIDS']['Filename'])
+                        fdel=file.get('info',{})
+                        del(fdel['IntendedFor'])
+                        file.update_info(fdel)
+                    else:
+                        print('no',file.info['BIDS']['Filename'])
+                        
+                        
+                        
+                        #print(file.info.keys())
+                        #file.update_info(file.info)
+                        print(file.info['IntendedFor'])
+                        
+                        
+                        
 filecheck = 'pennftdcenter/' + project + '/' + subs[s] + '/' + sesslist[s] 
 session=fw.lookup(filecheck)
 acqs=[a for a in session.acquisitions() if "SpinEchoFieldMap" in a.label]
 for acq in acqs:
         acq=acq.reload()
         for file in acq.files:
-            print(file)
+            print(file.info.keys())
 
-
-        for f,g in zip(files_json,files_json_fullpaths):
-            temp = re.findall(r'\d+', f)
-            if int(temp[-1]) == runs[-1]:
-                items = f.split('_')
-                if 'dir-AP' in items:
-                    with open(g,'r')as f_json:
-                        data=json.load(f_json)
-                        data["IntendedFor"]=["func/{}_{}_task-gambling_dir-PA_bold.nii.gz".format(subj,s$
-                                             "func/{}_{}_task-WM_dir-PA_bold.nii.gz".format(subj,ses)]
-                        f_json.close
-                    with open(g,'w')as f_json:
-                        json.dump(data,f_json,indent=4,sort_keys=True)
-                        f_json.close
-                elif 'dir-PA' in items:
-                    with open(g,'r')as f_json:
-                        data=json.load(f_json)
-                        data["IntendedFor"]=["func/{}_{}_task-gambling_dir-AP_bold.nii.gz".format(subj,s$
-                                             "func/{}_{}_task-WM_dir-AP_bold.nii.gz".format(subj,ses)]
-                        f_json.close
-                    with open(g,'w')as f_json:
-                        json.dump(data,f_json,indent=4,sort_keys=True)
-                        f_json.close
